b546fac581381012e6533cd224ac567a3cf38f0d
carterave519 <carterave519@gmail.com>
2018-05-09 15:56:44 -0400

    second commit

---
+++ Capstone2TaskList/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone2TaskList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Task Manager!");
+
+            List<Task> TaskList = new List<Task>();
+
+            TaskList.Add(new Task("Brian", "Update calendar"));
+            TaskList.Add(new Task("Pai'Tyn", "Alpabatize files"));
+            TaskList.Add(new Task("Kylie", "Order supplies"));
+            TaskList.Add(new Task("Karter", "Perform student count"));
+            TaskList.Add(new Task("Kennedi", "Start new client list"));
+
+
+            while (true)
+            {
+                //display list of options
+                string input = showMenu();
+                if (input == "1")
+                {
+                    //display list of tasks and their information
+                    Task.DisplayList(TaskList);
+                }
+                //what conditions need to be met
+                else if(input == "2")
+                {
+                    //create a new task and add it to the list
+                    Task ToAdd = Task.CreateTask();
+                    TaskList.Add(ToAdd);
+                }
+                else if(input == "3")
+                {
+                    //delete a task based on user input
+                    TaskList = Task.DeleteTask(TaskList);
+                }
+                else if(input == "4")
+                {
+                    //complete a task based on user input
+                    TaskList = Task.CompleteTask(TaskList);
+                   
+                }
+                else if(input == "5")
+                {
+                    //exit program
+                    break;
+                }
+            }
+
+            Console.WriteLine("Goodbye!");
+
+
+        }
+
+        static string showMenu()
+        {
+            Console.WriteLine("1. List tasks\n2. Add task\n3. Delete task\n4. Mark task complete\n5. Quit\n" );
+
+            Console.WriteLine("What do you want to do?");
+
+            string input = Console.ReadLine();
+            return input;
+        }
+         
+        
+
+        
+    }
+}
---
+++ Capstone2TaskList/Task.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone2TaskList
+{
+    class Task
+    {
+        public string TaskOwner { get; set; }
+        public string TaskName { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool completeTask { get; set; }
+
+        //Constructor
+        public Task(string taskOwner, string taskName)
+        {
+            TaskOwner = taskOwner;
+            TaskName = taskName;
+            completeTask = false;
+            DueDate = DateTime.Now.AddDays(14);
+        }
+
+        //no ; bc it's a method (it stands alone)
+        //List of type task
+        public static void DisplayList(List<Task> TaskList)
+        {
+            Console.WriteLine("Done?\t\tDue Date\t\tTeam Member\tDescription");
+            //display each task on a new line
+            foreach (Task item in TaskList)
+            {
+                Console.WriteLine($"{item.completeTask}\t\t {item.DueDate:MM/dd/yyyy}\t\t{item.TaskOwner}\t\t{item.TaskName}");
+
+            }
+            Console.WriteLine();
+        }
+
+        //returns a task object
+        public static Task CreateTask()
+        {
+            //take input for the new task
+            Console.WriteLine("Team Member Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Description");
+            string description = Console.ReadLine();
+            //return the new task
+            return new Task(name, description);
+        }
+
+        public static List<Task> DeleteTask(List<Task> TaskList)
+        {
+            //display each task on a new line
+            int count = 1;
+            foreach(Task item in TaskList)
+            {
+                Console.WriteLine($"{count}\t\t{item.TaskOwner}\t\t{item.TaskName}");
+                count++;
+            }
+            //take input from user
+            Console.WriteLine("What task do you want to delete");
+
+            //Starting at 1 for user/ -1 is to retrieve index
+            int input = int.Parse(Console.ReadLine())-1;
+            //initializing input as a parsed value of what the user puts into the Console
+            //remove task from list based on user's input
+            TaskList.RemoveAt(input);
+            return TaskList;
+        }
+
+        public static List<Task> CompleteTask(List<Task> TaskList)
+        {
+            //display each task on a new line
+            int count = 1;
+            foreach (Task item in TaskList)
+            {
+                Console.WriteLine($"{count}\t\t{item.TaskOwner}\t\t{item.TaskName}");
+                count++;
+            }
+            //take input from user
+            Console.WriteLine("What task do you want to mark complete");
+
+            //Starting at 1 for user/ -1 is to retrieve index
+            int input = int.Parse(Console.ReadLine()) - 1;
+            //initializing input as a parsed value of what the user puts into the Console
+            //Referencing an index of the List
+            //change completion status of task based on user input
+            TaskList[input].completeTask = true;
+            return TaskList;
+        }
+    }
+}
